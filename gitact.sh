68cd2eed4f18d07f09cc37577f12cf2bffc94a3f
leung7963 <104255180+leung7963@users.noreply.github.com>
2024-11-12 08:31:42 +0800

    更新 gitact.sh

---
+++ gitact.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+token="YOUR_PERSONAL_ACCESS_TOKEN"
+repo="your - username/your - repository"
+workflow_file="main.yml"
+api_url="https://api.github.com/repos/$repo/actions/workflows/$workflow_file/dispatches"
+data='{"ref": "main"}' # 假设你的工作流在main分支，根据实际修改分支名称
+curl -X POST -H "Authorization: token $token" -H "Content-Type: application/json" -d "$data" $api_url
